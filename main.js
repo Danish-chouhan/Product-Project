9f9050a561d46f85e13b61e73b736f3e186fcaa5
Danish-chouhan <danishchouhan41@gmail.com>
2024-01-03 02:55:40 +0530

    static to dynamic Done

--- main.js
+++ main.js
@@ -1,24 +1,52 @@
-const form = document.querySelector("form")
+const productList = [];
 
+const form = document.querySelector("form");
 
-
-const productPrice = document.getElementById("Product-Price")
-const productName = document.getElementById("Product-Name")
-const productQuentity = document.getElementById("Product-Quentity")
-const mrp = document.getElementById("MRP")
-const currentRate = document.getElementById("Current-Rate")
-form.addEventListener("submit",(ele)=>{
-  ele.preventDefault()
+const productPrice = document.getElementById("Product-Price");
+const productName = document.getElementById("Product-Name");
+const productQuentity = document.getElementById("Product-Quentity");
+const mrp = document.getElementById("MRP");
+const currentRate = document.getElementById("Current-Rate");
+form.addEventListener("submit", (ele) => {
+  ele.preventDefault();
   const Product = {
-      Price : productPrice.value,
-      Name : productName.value,
-      Quentity : productQuentity.value,
-      mrp : mrp.value,
-      Rate : currentRate.value
-  }
-  const productJSON = JSON.stringify(Product);
+    Price: productPrice.value,
+    Name: productName.value,
+    Quentity: productQuentity.value,
+    mrp: mrp.value,
+    Rate: currentRate.value,
+  };
+  productList.push(Product);
+  const productListData = JSON.stringify(productList);
 
-  localStorage.setItem("productData", productJSON);
+  localStorage.setItem("productData", productListData);
+});
 
-  console.log("Product saved in local storage:", Product);
-})
+// --------------------Products
+
+const body = document.querySelector("body");
+const dataArray = JSON.parse(localStorage.getItem("productData"));
+
+const mainContainerOfProduct = document.createElement("div");
+mainContainerOfProduct.classList.add("maiContainer");
+body.appendChild(mainContainerOfProduct);
+
+dataArray.forEach(element => {
+  const containerProduct = createAndAppendProduct("div", "Product", mainContainerOfProduct);
+
+  createAndAppendProduct("h2", "ProductPrice", containerProduct, `<span id="Rupee">₹</span>${element.Price}<span id="Only">only</span>`);
+  createAndAppendProduct("p", "ProductName", containerProduct, `${element.Name}, ${element.Quentity}`);
+
+  const containerOfPrice = createAndAppendProduct("div", "price", containerProduct);
+
+  createAndAppendProduct("p", null, containerOfPrice, `MRP Rs : <del>${element.mrp}</del>`);
+  createAndAppendProduct("p", null, containerOfPrice, `Price : <span id="Rupee">₹</span>${element.Rate}`);
+});
+
+function createAndAppendProduct(tag, className, parent, innerHTML) {
+  const element = document.createElement(tag);
+  if (className) element.classList.add(className);
+  if (innerHTML) element.innerHTML = innerHTML;
+  parent.appendChild(element);
+  return element;
+}
